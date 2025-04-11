851a745f70b48ab0dcbb64cc4ff1bb9a538543ad
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-11 15:01:55 +0300

    added

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import grid1 from '../../assets/images/Shared goals-bro 1 (1).png'
+import grid1 from "../../assets/images/Shared goals-bro 1 (1).png";
+import grid2 from "../../assets/images/Typewriter-bro 1.png";
+import grid3 from "../../assets/images/House bookshelves-bro 1.png";
+import grid4 from "../../assets/images/";
 
 const Skills = () => {
   return (
@@ -9,9 +12,15 @@ const Skills = () => {
         <div class="div1">
           <img src={grid1} alt="grid1" />
         </div>
-        <div class="div2">2</div>
-        <div class="div3">3</div>
-        <div class="div4">4</div>
+        <div class="div2">
+          <img src={grid2} alt="grid2" />
+        </div>
+        <div class="div3">
+          <img src={grid3} alt="grid3" />
+        </div>
+        <div class="div4">
+          <img src={grid4} alt="grid4" />
+        </div>
       </div>
     </div>
   );
