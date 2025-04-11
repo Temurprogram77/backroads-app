7d434d7043644c9058643d1621e860a8cecc2a69
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-11 14:28:55 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from "react-dom/client";
+import Navbar from './components/navbar/navbar';
+import Header from './components/header/header';
+import Skills from './components/skills/skills';
+import About from './components/about/about';
+import Pricing from './components/pricing/pricing';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
 
 const Index = () => {
   return (
     <div>
       <Navbar />
-      
+      <Header />
+      <Skills />
+      <About />
+      <Pricing />
+      <Contact />
+      <Footer />
     </div>
   )
 }
