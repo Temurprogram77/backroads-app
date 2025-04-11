eab18ad8c621b7bcb6f91c37256a00d1f88c5973
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-11 14:59:21 +0300

    added

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import grid1 from '../../assets/images/Shared goals-bro 1 (1).png'
 
 const Skills = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Skills</h2>
-      <div className=""></div>
+      <div class="parent">
+        <div class="div1">
+          <img src={grid1} alt="grid1" />
+        </div>
+        <div class="div2">2</div>
+        <div class="div3">3</div>
+        <div class="div4">4</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
