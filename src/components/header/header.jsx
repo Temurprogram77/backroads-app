71dc0991aa10232ed555815f01879cf553bd4f0c
Temur Coder <temurnarzullayev810@gmail.com>
2025-04-11 14:46:01 +0300

    added

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="container header">
+      <div className="left-part-header">
+        <h2>Learn Any Foreign Language</h2>
+        <p>
+          With our teachers who write a program for each student, you will be
+          able to make your first sketch after the first lesson.
+        </p>
+        <div className="button">Get started</div>
+      </div>
+      <div className="right-part-header"></div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
